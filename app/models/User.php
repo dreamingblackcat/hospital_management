ae113969d3e624ee153922c7589ec3b23b9b07d8
dreamingblackcat10 <dreamingblackcat10@gmail.com>
2014-11-12 17:49:41 +0630

    add remember_token field to user model so that UserInterface is implemented correctly

--- app/models/User.php
+++ app/models/User.php
@@ -2,12 +2,12 @@
 
 use Illuminate\Auth\UserTrait;
 use Illuminate\Auth\UserInterface;
-use Illuminate\Auth\Reminders\RemindableTrait;
-use Illuminate\Auth\Reminders\RemindableInterface;
+// use Illuminate\Auth\Reminders\RemindableTrait;
+// use Illuminate\Auth\Reminders\RemindableInterface;
 
-class User extends Eloquent implements UserInterface, RemindableInterface {
+class User extends Eloquent implements UserInterface {
 
-	use UserTrait, RemindableTrait;
+	use UserTrait;
 
 	/**
 	 * The database table used by the model.
@@ -22,5 +22,10 @@ class User extends Eloquent implements UserInterface, RemindableInterface {
 	 * @var array
 	 */
 	protected $hidden = array('password', 'remember_token');
+
+	public function getRememberToken()
+	{
+		return 'remember_token';
+	}
 
 }
