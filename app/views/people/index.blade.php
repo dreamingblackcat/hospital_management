12edcc2ec8f13aa44efaf32911c203e7d6df61c4
dreamingblackcat10 <dreamingblackcat10@gmail.com>
2014-11-13 16:23:07 +0630

    added index page html

--- app/views/people/index.blade.php
+++ app/views/people/index.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.master')
+@extends('layouts.default')
 <!-- 
 @section('sidebar')
     @parent
@@ -7,8 +7,37 @@
 @stop -->
 
 @section('content')
-    @foreach ($person as $people)
-    <p>Name: {{ $people->name }}</p>
-@endforeach
+<div class="alc">
+	<h3>List of People</h3>
+</div>
+<div class="container">
+<table class="table-bordered">
+	<thead>
+		<tr>
+			<th>Name</th>
+			<th>Nationality</th>
+			<th>Ethnic Group</th>
+			<th>Date of Birth</th>
+			<th>NRC Number</th>
+			<th>Contact Email</th>
+			<th>Phone Number</th>
+		</tr>
+	</thead>
+    @foreach ($people as $person)
+    <tr>
+    	<td>{{ $person->name }}</td>
+    	<td>{{ $person->nationality }}</td>
+    	<td>{{ $person->ethnic_group }}</td>
+    	<td>{{ $person->date_of_birth->format('Y-m-d') }}</td>
+    	<td>{{ $person->nrc_number }}</td>
+    	<td>{{ $person->contact_email }}</td>
+    	<td>{{ $person->phone_number }}</td>
+    </tr>
+	@endforeach
+</table>
+</div>
+<div class="alr">
+	{{ link_to_route('people.create', "New Pesron", $parameters = array(), $attributes = array('class' => 'btn btn-default'))}}
+</div>
 
 @stop
