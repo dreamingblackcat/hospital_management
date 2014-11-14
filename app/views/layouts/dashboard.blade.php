f8870213709fc360fba2fd17f7505fcce45b7092
dreamingblackcat10 <dreamingblackcat10@gmail.com>
2014-11-14 15:00:12 +0630

    added dashboard layout and fixed people index to use the layout

---
+++ app/views/layouts/dashboard.blade.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Hospital Management | Sign in</title>		
+	{{ Bootstrap::css('local', ['type' => 'text/css']);}}
+	{{ HTML::style('assets/css/font-awesome.min.css'); }}
+<!-- 	{{ HTML::style('assets/css/bootstrap-theme.min.css'); }}
+ -->	{{ HTML::style('assets/css/log_in_page.css'); }}
+ -->	{{ HTML::style('assets/css/cmsh.css'); }}
+</head>
+<body>
+	<div class="main-container container-fluid">
+		<div class="sidebar cm-sidebar" id="sidebar">
+			<ul class="nav nav-list">
+				<li class="active row">
+					<div class="col-sm-6 alc">
+						<i class="fa fa-dashboard fa-2x"></i>
+					</div>
+					<div class="col-sm-6">
+						<a href="{{ URL::route('people.index')}}">
+							
+							<span class="menu-text"> People </span>
+						</a>
+					</div>
+				</li>
+
+				<li class="row">
+					<div class="col-sm-6 alc">
+						<i class="fa fa-group fa-2x"></i>
+					</div>
+					<div class="col-sm-6">
+						<a href="{{ URL::route('people.index')}}">
+							
+							<span class="menu-text"> People </span>
+						</a>
+					</div>
+				</li>
+				<li class="row">
+					<div class="col-sm-6 alc">
+						<i class="fa fa-eye fa-2x"></i>
+					</div>
+					<div class="col-sm-6">
+						<a href="{{ URL::route('people.index')}}">
+							
+							<span class="menu-text"> People </span>
+						</a>
+					</div>
+				</li>
+				<li class="row">
+					<div class="col-sm-6 alc">
+						<i class="fa fa-user fa-2x"></i>
+					</div>
+					<div class="col-sm-6">
+						<a href="{{ URL::route('people.index')}}">
+							
+							<span class="menu-text"> People </span>
+						</a>
+					</div>
+				</li>
+				
+			</ul><!--/.nav-list-->
+
+			<div class="sidebar-collapse" id="sidebar-collapse">
+				<i class="icon-double-angle-left"></i>
+			</div>
+		</div>
+		<div class="content">
+			<div class="col-md-12">
+				<div>
+					@yield('heading')
+				</div>
+				<div class="panel panel-default">
+				  <div class="panel-heading cm-panel-heading">
+				  	@yield('sub_heading')
+				  </div>
+				  <div class="panel-body cm-panel-color">
+				    @yield('content')
+				  </div>
+				</div>
+			</div>
+			
+		</div>
+	</div>
+	{{ Bootstrap::js('local', ['type' => 'text/javascript']); }}
+</body>
+</html>
+
--- app/views/people/index.blade.php
+++ app/views/people/index.blade.php
@@ -1,19 +1,31 @@
-@extends('layouts.default')
+@extends('layouts.dashboard')
 <!-- 
 @section('sidebar')
     @parent
 
     <p>This is appended to the master sidebar.</p>
 @stop -->
+@section('heading')
+	<h3>People</h3>
+@stop
+
+@section('sub_heading')
+	<div class="row">
+		<div class="col-sm-4 sub-heading">
+			People List
+		</div>
+		<div class="col-sm-offset-4 col-sm-4">
+			<input type="text" class="form-control" placeholder="Text input">
+		</div>
+	</div>
+@stop
 
 @section('content')
-<div class="alc">
-	<h3>List of People</h3>
-</div>
-<div class="container">
-<table class="table-bordered">
+<div class="col-md-12">
+<table class="cm-table table-bordered">
 	<thead>
 		<tr>
+			<th>No.</th>
 			<th>Name</th>
 			<th>Nationality</th>
 			<th>Ethnic Group</th>
@@ -21,10 +33,12 @@
 			<th>NRC Number</th>
 			<th>Contact Email</th>
 			<th>Phone Number</th>
+			<th colspan="3">Actions</th>
 		</tr>
 	</thead>
-    @foreach ($people as $person)
+    @foreach ($people as $index => $person)
     <tr>
+    	<td>{{ $index+1 }}</td>
     	<td>{{ $person->name }}</td>
     	<td>{{ $person->nationality }}</td>
     	<td>{{ $person->ethnic_group }}</td>
@@ -32,12 +46,15 @@
     	<td>{{ $person->nrc_number }}</td>
     	<td>{{ $person->contact_email }}</td>
     	<td>{{ $person->phone_number }}</td>
+    	<td>{{ link_to_route('people.edit', "Edit", $parameters = array('id'=>$person->id), $attributes = array('class' => 'btn btn-default')) }}</td>
+    	<td>{{ link_to_route('people.show', "View", $parameters = array('id'=>$person->id), $attributes = array('class' => 'btn btn-default')) }}</td>
+    	<td>{{ link_to_route('people.destroy', "Delete", $parameters = array('id'=>$person->id), $attributes = array('class' => 'btn btn-default')) }}</td>
     </tr>
 	@endforeach
 </table>
 </div>
+<br>
 <div class="alr">
-	{{ link_to_route('people.create', "New Pesron", $parameters = array(), $attributes = array('class' => 'btn btn-default'))}}
 </div>
 
 @stop
