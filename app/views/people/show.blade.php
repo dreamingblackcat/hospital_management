8a2284221c729e24079c3d5b7777629c8b549231
dreamingblackcat10 <dreamingblackcat10@gmail.com>
2014-11-18 13:59:46 +0630

    added show page skeleton for people

--- app/views/people/show.blade.php
+++ app/views/people/show.blade.php
@@ -1,1 +1,27 @@
-/media/dbc/Data/Projects/PhpProjects/LaravelApps/hospital/resources/views/people/show.blade.php
+@extends('layouts.dashboard')
+
+@section('heading')
+	<h3>Personal Information of {{ $person->name }}</h3>
+@stop
+
+@section('sub_heading')
+	<div class="row">
+		<div class="col-sm-4 sub-heading">
+			{{ $person->name }}
+		</div>
+		<div class="col-sm-offset-4 col-sm-4">
+			<input type="text" class="form-control" placeholder="Text input">
+		</div>
+	</div>
+@stop
+
+@section('content')
+		<div class="alc">			
+			<p>{{ $person->name }}</p>
+			<p>{{ $person->nationality }}</p>
+			<p>{{ $person->ethnic_group }}</p>
+			<p>{{ $person->date_of_birth->format('Y-m-d') }}</p>
+			<p>{{ $person->nrc_number }}</p>
+			<p>{{ $person->phone_number }}</p>
+		</div>
+@stop
