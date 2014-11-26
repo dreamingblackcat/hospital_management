bf0f18b067064271955967900af63c1c518bb55a
dreamingblackcat10 <dreamingblackcat10@gmail.com>
2014-11-26 16:20:20 +0630

    fixed FormMacros to support single nested attribute form

--- app/Awitd/Macros/FormMacros.php
+++ app/Awitd/Macros/FormMacros.php
@@ -3,9 +3,10 @@
 use Illuminate\Html\FormBuilder;
 
 Form::macro('bsTextFieldWithLabel', function($column_name,$object,$errors,$old_value){
-	$label_text = ucwords($column_name);
-	if($errors->has($column_name)){
-		$error_msg = $errors->first($column_name);
+	$sanitized_column_name = sanitizeColumnName($column_name);
+	$label_text = ucwords($sanitized_column_name);
+	if($errors->has($sanitized_column_name)){
+		$error_msg = $errors->first($sanitized_column_name);
 		$error_text = "<p class='alert alert-warning'>$error_msg</p>";
 	}else{
 		$error_text = "";
@@ -13,15 +14,37 @@ Form::macro('bsTextFieldWithLabel', function($column_name,$object,$errors,$old_v
 			
 	$html = <<<FORM_CONTROL
 	<div class="form-group">
-	    <label class="col-sm-4" for="$column_name">
+	    <label class="col-sm-4" for="$sanitized_column_name">
 	        $label_text:
 	    </label>
 	    <div class="col-sm-5">
-	        <input id="$column_name" class="form-control" type="text" value="$old_value" name="$column_name"></input>
+	        <input id="$sanitized_column_name" class="form-control" type="text" value="$old_value" name="$column_name"></input>
 	    	$error_text
 		</div>
 	</div>
 FORM_CONTROL;
-
 return $html;
 });
+
+// TODO => helper functions without tests
+//         must rewrite for various use cases
+
+function sanitizeColumnName($column_name){
+	$start = strpos($column_name, '[');
+	$label_text = '';
+	if($start){
+		$end = strpos($column_name,']');
+		$label_text = extractNestedColumnName($column_name,$start,$end);
+	}else{
+		$label_text = $column_name;
+	}
+	return $label_text;
+}
+
+function extractNestedColumnName($str,$start,$end){
+	$nested_data = '';
+	for($i= $start+1; $i < $end; $i++ ){
+		$nested_data .= $str[$i];
+	}
+	return $nested_data;
+}
