0e190a43e77798f84a8e8eda99681846cf0287a4
dreamingblackcat10 <dreamingblackcat10@gmail.com>
2014-12-01 13:18:18 +0630

    added create link in doctors index

--- app/views/doctors/index.blade.php
+++ app/views/doctors/index.blade.php
@@ -15,7 +15,16 @@
 			Doctor List
 		</div>
 		<div class="col-sm-offset-4 col-sm-4">
-			<input type="text" class="form-control" placeholder="Text input">
+			<div class="row">
+				<div class="col-sm-9">
+					<input type="text" class="form-control" placeholder="Text input">
+
+				</div>
+				<div class="col-sm-3">
+					{{ link_to_route('doctors.create', "New",array(), $attributes = array('class' => 'btn btn-default')) }}
+				</div>
+			</div>
+			
 		</div>
 	</div>
 @stop
